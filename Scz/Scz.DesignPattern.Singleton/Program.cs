fea77d185ccd4c56c255169a9f60c56dc10aa9bf
365848719 <44990364+365848719@users.noreply.github.com>
2018-12-24 15:22:08 +0800

    添加单例模式

--- Scz/Scz.DesignPattern.Singleton/Program.cs
+++ Scz/Scz.DesignPattern.Singleton/Program.cs
@@ -6,7 +6,19 @@ namespace Scz.DesignPattern.Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SingleDemo s1 = SingleDemo.GetInstance();
+            SingleDemo s2 = SingleDemo.GetInstance();
+
+            if (s1.Equals(s2))
+            {
+                Console.WriteLine("s1 == s2");
+            }
+            else
+            {
+                Console.WriteLine("两个对象不等。。。。");
+            }
+
+            Console.ReadKey();
         }
     }
 }

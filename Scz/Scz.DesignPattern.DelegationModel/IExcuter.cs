259ebd7d5313b5c629c24479edf9bb97495aeee1
365848719 <44990364+365848719@users.noreply.github.com>
2019-01-10 17:32:09 +0800

    修改委派模式

--- Scz/Scz.DesignPattern.DelegationModel/IExcuter.cs
+++ Scz/Scz.DesignPattern.DelegationModel/IExcuter.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Scz.DesignPattern.DelegationMode
 {
+    /// <summary>
+    /// 委派模式
+    /// </summary>
     public interface IExcuter
     {
         void Exec(string command);

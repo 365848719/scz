55059e2d0e92fd0f2e114bac06c673f2f2203edb
施成志 <365848719@qq.com>
2019-10-16 23:43:33 +0800

    添加aop 文件

---
+++ Scz/Scz.Aop/OrderProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scz.Aop
+{
+    public class OrderProcessor : IOrderProcessor
+    {
+        public void Submit(Order order)
+        {
+            Console.WriteLine("提交订单");
+        }
+    }
+}

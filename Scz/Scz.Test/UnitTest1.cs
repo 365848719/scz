db0531b6c7df3fad2df3a6845c66172d28825323
施成志 <365848719@qq.com>
2019-10-18 16:49:33 +0800

    添加测试用例

--- Scz/Scz.Test/UnitTest1.cs
+++ Scz/Scz.Test/UnitTest1.cs
@@ -11,4 +11,35 @@ namespace Scz.Test
 
         }
     }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public interface IUserRepository
+    {
+        bool Add(User user);
+    }
+
+    public class UserManager
+    {
+        IUserRepository userRepository;
+
+        public UserManager(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public bool CreateUser(User user)
+        {
+            if (user.Age < 0 || string.IsNullOrEmpty(user.Name))
+            {
+                throw new FormatException("Age or Name is inValid ! ");
+            }
+
+            return true;
+        }
+    }
 }
